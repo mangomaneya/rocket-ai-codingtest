09708b3eddf0ec1f69b9f0a6c0339b0d72209a2f
lee kiri <mango.man.e.ya@gmail.com>
2025-06-18 22:07:25 +0900

    Chore: add Tailwind CSS configuration with custom font family

---
+++ tailwind.config.ts
@@ -0,0 +1,12 @@
+import type { Config } from "tailwindcss";
+
+const config: Config = {
+  theme: {
+    extend: {
+      fontFamily: {
+        zen: ["var(--font-zen-serif)"],
+      },
+    },
+  },
+
+};
